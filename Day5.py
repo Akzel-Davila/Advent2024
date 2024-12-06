9cb3507fe5e89ba35ddab41d4ee7cac50196f6b2
Akzel-Davila <145377323+Akzel-Davila@users.noreply.github.com>
2024-12-06 14:47:16 -0500

    Add files via upload

---
+++ Day5.py
@@ -0,0 +1,27 @@
+def get_file_data(file_name):
+    f = open(file_name)
+    data = []
+    for line in f:
+        data.append(line.rstrip())
+    return data
+
+
+file_data = get_file_data("Day5Input.txt")
+# you now have a list of Strings from the input file
+page_pairs = file_data[:file_data.index("")]
+ordered= []
+print(page_pairs)
+
+for pair in page_pairs:
+    first = pair[:pair.index("|")]
+    second = pair[pair.index("|")+1:]
+    try:
+        ordered.index(second)
+    except:
+        ordered.append(second)
+    try:
+        ordered.index(first)
+    except:
+        ordered.append(first)
+    #Move the first to behind the second. Pop the occurence of first beforehand.
+print(ordered)
