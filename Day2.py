661d7db1550a864110dd4c886db7b6b88ded7369
Akzel-Davila <145377323+Akzel-Davila@users.noreply.github.com>
2024-12-10 14:49:29 -0500

    Add files via upload

--- Day2.py
+++ Day2.py
@@ -9,21 +9,19 @@ def get_file_data(file_name):
 def day2_solution(data):
     safe_reports = 0
     for rules in data:
+        safe = False
         rules = rules.split(" ")
-        rules = helper(rules)
-        if(rules == rules.sort() or rules == rules.reverse()):
+        if(rules == sorted(rules) or rules == rules[::-1]):
             for i in range(len(rules)-1):
                 difference = abs(int(rules[i])-int(rules[i+1]))
-                print(difference)
-                if(difference<=3 and difference>1):
-                    safe_reports +=1
+                if (difference<=3 and difference>=1):
+                    safe = True
+                else:
+                    safe= False
+            if(safe):
+                safe_reports+=1
     print(safe_reports)
 
-def helper(rules):
-    new_list = []
-    for i in range(len(rules)):
-        new_list.append(int(rules[i]))
-    return new_list
 
 file_data = get_file_data("Day2Input")
 # you now have a list of Strings from the input file
