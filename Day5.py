26a60c4e0adc5deaf78db0257fc93361b32bb518
Akzel-Davila <145377323+Akzel-Davila@users.noreply.github.com>
2024-12-16 14:46:10 -0500

    Add files via upload

--- Day5.py
+++ Day5.py
@@ -1,4 +1,3 @@
-
 def get_file_data(file_name):
     f = open(file_name)
     data = []
@@ -9,23 +8,20 @@ def get_file_data(file_name):
 
 file_data = get_file_data("Day5Input.txt")
 # you now have a list of Strings from the input file
-page_pairs = file_data[file_data.index("")+1:]
+page_rules = file_data[file_data.index("")+1:]
 ordered= []
-print(page_pairs)
+print(page_rules)
 
-def part1_solution(data):
-    for pair in page_pairs:
-        first = pair[:pair.index("|")]
-        second = pair[pair.index("|")+1:]
-        try:
-            ordered.index(second)
-        except:
-            ordered.append(second)
-        try:
-            ordered.index(first)
-        except:
-            ordered.append(first)
-    #Move the first to behind the second. Pop the occurence of first beforehand.
+def part1_solution(file_data):
+    page_rules = file_data[file_data.index("") + 1:]
+    page_list = file_data[:file_data.index("")]
+    print(page_list)
+    for pair in page_rules:
+        num_list = pair.split(",")
+        print(num_list)
+        for num in num_list:
+            #Compare the num to the string of the page pairs
 
-    #
-#print(ordered)
+
+
+part1_solution(file_data)
