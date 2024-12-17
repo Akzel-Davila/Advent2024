f312d1d21268f654e0ba59eb1942ab351a51bda1
Akzel-Davila <145377323+Akzel-Davila@users.noreply.github.com>
2024-12-17 14:48:59 -0500

    Add files via upload

--- Day5.py
+++ Day5.py
@@ -8,19 +8,26 @@ def get_file_data(file_name):
 
 file_data = get_file_data("Day5Input.txt")
 # you now have a list of Strings from the input file
-page_rules = file_data[file_data.index("")+1:]
-ordered= []
-print(page_rules)
+
 
 def part1_solution(file_data):
     page_rules = file_data[file_data.index("") + 1:]
     page_list = file_data[:file_data.index("")]
-    print(page_list)
+    new_pairs = []
     for pair in page_rules:
         num_list = pair.split(",")
-        print(num_list)
-        for num in num_list:
-            #Compare the num to the string of the page pairs
+        for i in range(len(page_list)):
+            num_elements = 0
+            for num in num_list:
+                if page_list[i].__contains__(num):
+                    num_elements += 1
+            if num_elements>1:
+                new_pairs.append(page_list[i])
+        for pairs in new_pairs:
+            print(pairs)
+
+    print(new_pairs)
+
 
 
 
